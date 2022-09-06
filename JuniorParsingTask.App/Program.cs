845c61281d4eee75f25e515b5a1978d720826794
Piotr Kopciewicz <piotrkopciewicz@gmail.com>
2022-09-06 07:21:48 +0200

    Dodanie funkcji przeszukującej drzewo

--- JuniorParsingTask.App/Program.cs
+++ JuniorParsingTask.App/Program.cs
@@ -1,6 +1,65 @@
 ﻿using JuniorParsingTask;
 
+
 //tree for searching
 var tree = TreeService.Create();
+
+bool TryGetNode(string value, out Node node) 
+{
+    if (tree != null)
+    {
+        Stack<Node> stack = new();
+        node = tree.Root;
+        foreach(Node n in node.Children)
+        {
+            stack.Push(n);
+        }
+        while (stack.Count != 0) 
+        {
+            Node parent = stack.Pop();
+            if(parent.Value == value)
+            {
+                node = parent;
+                Console.WriteLine("value found, success");
+                return true;
+            }
+            else
+            {
+                foreach(Node n in parent.Children)
+                {
+                    if (n.Value == value)
+                    {
+                        node = n;
+                        Console.WriteLine("value found, success");
+                        return true;
+                    }
+                    else
+                    {
+                        stack.Push((Node)n);
+                    }
+                }
+            }
+        }
+        node = null!;
+        Console.WriteLine("value NOT found :(");
+        return false;
+    }
+    else
+    {
+        node = null!;
+        Console.WriteLine("tree = null");
+        return false;
+    }
+}
+
+Console.WriteLine("Type searched string: ");
+string? str = Console.ReadLine();
+
+if (str != null)
+{
+    Node nod;
+    TryGetNode(str, out nod);
+}
+else
 
 Console.ReadKey();
